03c815a9b01649cbf1f7d665c282f139972ea1a3
Amey-274 <nonhackable1290@gmail.com>
2021-01-25 08:47:40 +0530

    add some stuff

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/Myamazon',{
+mongoose.connect(process.env.MONGOURI,{
     useFindAndModify:false,
     useNewUrlParser:true,
     useUnifiedTopology:true,
