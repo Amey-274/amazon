5daf45fbad0554f2af0da1ce16a13acef418e145
Amey-274 <nonhackable1290@gmail.com>
2021-01-25 10:54:17 +0530

    server js add text

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express')
 const app = express()
 const data = require('./data')
 const orderRouter = require('./routers/orderRouter')
-const port = process.env.PORT|| 5001
+const port = process.env.PORT|| 5002
 const ProductRouter = require('./routers/productRouter')
 const userRouter  = require('./routers/userRouter')
+const path = require('path');
 require('./models/db')
 require('dotenv').config()
 
@@ -12,10 +13,12 @@ require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded())
 
-app.get('/',(req,res)=>{
-    res.send(" server is ready ")
+app.use(express.static(path.join(__dirname, './client/build')));
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
 })
-           
+
 app.use('/api/products',ProductRouter)
 app.use('/api/user',userRouter)
 app.use('/api/orders',orderRouter)
@@ -25,14 +28,8 @@ app.use('/api/orders',orderRouter)
 app.use((err,req,res,next)=>{
     res.status(500).send({message:err.message})
 })
-if(process.env.NODE_ENV=="production"){
-    app.use(express.static('../frontend/build'))
-    const path = require('path')
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
-}
+
 app.listen(port,()=>{
-    console.log(`http://localhost:${port}/api/products/data`)
+    console.log(`http://localhost:${port}/`)
 })  
 
